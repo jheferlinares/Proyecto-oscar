60d98b0077577e2b34dea4fa6786bcce3bb836a6
jheferlinares <jhefer98@hotmail.com>
2025-11-22 16:33:09 -0400

    test

--- models/ModeloComputadora.js
+++ models/ModeloComputadora.js
@@ -4,7 +4,7 @@ const ModeloComputadoraSchema = new mongoose.Schema({
   nombre: { type: String, required: true },
   codigo: { type: String, required: true, unique: true },
   tipo: { type: String, enum: ['Desktop', 'Laptop', 'Servidor', 'All-in-One'], default: 'Desktop' },
-  imagen: { type: String }, // ruta relativa dentro de /public/uploads
+  imagen: { type: String }, 
   creadoPor: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   fechaCreacion: { type: Date, default: Date.now }
 });
